d3e7cdeba2dd234dcc11197d4baa6b7351733a17
Michael Diglio <mdiggss@gmail.com>
2017-05-02 23:05:45 -0400

    Steve Bachan Grid Site Creation
    Created website for Steve Bachan using the grid system. Base layout and
    site created

---
+++ assets/js/functions.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+  $('.image1').click(function(){
+    var src = $(this).css('background-image');
+    src = src.replace('url(','').replace(')','').replace(/\"/gi, "");
+    window.open(src,'', 'width=800, height=600');
+  });
+  $('.image2').click(function(){
+    var src = $(this).css('background-image');
+    src = src.replace('url(','').replace(')','').replace(/\"/gi, "");
+    window.open(src,'', 'width=800, height=600');
+  });
+  $('.image3').click(function(){
+    var src = $(this).css('background-image');
+    src = src.replace('url(','').replace(')','').replace(/\"/gi, "");
+    window.open(src,'', 'width=800, height=600');
+  });
+  $('.image4').click(function(){
+    var src = $(this).css('background-image');
+    src = src.replace('url(','').replace(')','').replace(/\"/gi, "");
+    window.open(src,'', 'width=800, height=600');
+  });
+  $('.image5').click(function(){
+    var src = $(this).css('background-image');
+    src = src.replace('url(','').replace(')','').replace(/\"/gi, "");
+    window.open(src,'', 'width=800, height=600');
+  });
+  $('.image6').click(function(){
+    var src = $(this).css('background-image');
+    src = src.replace('url(','').replace(')','').replace(/\"/gi, "");
+    window.open(src,'', 'width=800, height=600');
+  });
+  $('.image7').click(function(){
+    var src = $(this).css('background-image');
+    src = src.replace('url(','').replace(')','').replace(/\"/gi, "");
+    window.open(src,'', 'width=800, height=600');
+  });
+  $('.image8').click(function(){
+    var src = $(this).css('background-image');
+    src = src.replace('url(','').replace(')','').replace(/\"/gi, "");
+    window.open(src,'', 'width=800, height=600');
+  });
+});
